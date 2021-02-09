a3b4cfa06ceace1eb0b49d4717cec3a3e6e3aa06
Hossein Moradi <ho3inmoradi1998@gmail.com>
2021-02-09 00:36:41 -0800

    first commit

---
+++ app/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Category;
+use App\Seller;
+
+class Product extends Model
+{
+
+    const available_product = 'available';
+    const unavailable_product = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailable(){
+        return $this->status == Product::available_product;
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+}
