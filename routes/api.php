95180811b557ab2a218ecaea6e95f581d96b7255
Hossein Moradi <ho3inmoradi1998@gmail.com>
2021-02-11 14:05:32 -0800

    5th commit

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,11 @@ Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]
 Route::resource('users','User\UserController' , ['except' => ['create','edit']]);
 
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+Route::resource('categories','Category\CategoryController',['except'=>['create','edit']]);
+
+Route::resource('products','Product\ProductController',['only'=>['index','show']]);
+
+Route::resource('transactions','Transaction\TransactionController',['only'=>['index','show']]);
+Route::resource('transactions.categories','Transaction\TransactionCategoryController',['only'=>['index']]);
+Route::resource('transactions.sellers','Transaction\TransactionSellerController',['only'=>['index']]);
